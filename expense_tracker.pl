021c85986ea873c2076a642712568f0ab5b79f77
Swapnil Raykar <swap612@gmail.com>
2020-07-31 22:43:09 +0530

    Added the Category wise expense report

--- expense_tracker.pl
+++ expense_tracker.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use Data::Dumper;
 
 $\="\n";
 
@@ -45,9 +46,12 @@ sub add_exp{
 }
 
 sub show_exp{
+	
+	print "--------------------------------------------";
 	printf "|%15s|%15s|%10s|\n", 'Item','Category','Amount';
 	print "|------------------------------------------|";
 	my $total=int(0);
+	my %cat_sum;
 	open FH, '<', $file or die $!; 
 	while( <FH>){
 		#print $_;
@@ -56,6 +60,7 @@ sub show_exp{
 		my $cat = shift @exp_entry;
 		my $amt = shift @exp_entry;
 		chomp $amt;
+		$cat_sum{$cat}+=int($amt);
 		$total+=int ($amt);
 	        printf "|%15s|%15s|%10s|", $tit,$cat,$amt;
 
@@ -65,12 +70,19 @@ sub show_exp{
 
 	print "|------------------------------------------|";
         printf "|%15s|%15s|%10s| \n", 'Total',"",$total;
-	print "|------------------------------------------|";
-	#	print "Category Wise Distribution\n"
+	print "--------------------------------------------";
+	print "\n";
+	print "****** Category Wise Distribution ******\n";
 
+	#	print "$_ => $cat_sum{$_}" for keys %cat_sum;
+	print "---------------------------------";
+	printf "|%15s  || %10s |\n","Category","Cat. Total";
+	print "|-------------------------------|";
+	printf "|%15s  => %10s | \n",$_, $cat_sum{$_} for keys %cat_sum;
+	print "---------------------------------";
 }
 
-print "Your Expense Tracker System.\nEnter your choice \n1. Add Expense \n2. Expense Analysis \n3. Reset the Expense Tracking";
+print "Your Expense Tracker System.\nEnter your choice \n1. Add Expense \n2. Expense Report \n3. Reset the Expense Tracking";
 my $ch = int(<STDIN>);
 print $ch;
 
@@ -79,7 +91,7 @@ if($ch == 1){
 
 }
 elsif($ch == 2){
-	print "Showing the Expense Analysis Report";
+	print "Displaying the Expense Report";
 	show_exp;
 }
 elsif($ch == 3){
